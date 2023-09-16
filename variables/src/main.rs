8f6738331b28d305ce007f1cc812ecc5f103ffc7
Vidyesh Churi <vidyesh95@gmail.com>
2023-09-16 16:29:08 +0530

    Variables in Rust

---
+++ variables/src/main.rs
@@ -0,0 +1,9 @@
+/*
+* Variables are immutable by default
+*/
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+}
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/churi/code/language/rust/rust_tutorial/variables/target/debug/variables: /Users/churi/code/language/rust/rust_tutorial/variables/src/main.rs
