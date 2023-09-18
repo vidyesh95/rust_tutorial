5ddfe146f70a16252ed367eb009afe87762d1d3e
Vidyesh Churi <vidyesh95@gmail.com>
2023-09-19 01:13:17 +0530

    Functions in Rust

---
+++ functions/target/debug/deps/functions-fa73adf74f7d66fc.d
@@ -0,0 +1,5 @@
+/Users/churi/code/language/rust/rust_tutorial/functions/target/debug/deps/functions-fa73adf74f7d66fc: src/main.rs
+
+/Users/churi/code/language/rust/rust_tutorial/functions/target/debug/deps/functions-fa73adf74f7d66fc.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/Users/churi/code/language/rust/rust_tutorial/functions/target/debug/functions: /Users/churi/code/language/rust/rust_tutorial/functions/src/main.rs
