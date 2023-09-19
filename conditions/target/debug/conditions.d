8c6bb8ae948e4a2379c69111d3a5ee13e28bab47
Vidyesh Churi <vidyesh95@gmail.com>
2023-09-19 22:59:50 +0530

    Conditions in Rust

---
+++ conditions/target/debug/conditions.d
@@ -0,0 +1,1 @@
+/Users/churi/code/language/rust/rust_tutorial/conditions/target/debug/conditions: /Users/churi/code/language/rust/rust_tutorial/conditions/src/main.rs
---
+++ conditions/target/release/conditions.d
@@ -0,0 +1,1 @@
+/Users/churi/code/language/rust/rust_tutorial/conditions/target/release/conditions: /Users/churi/code/language/rust/rust_tutorial/conditions/src/main.rs
