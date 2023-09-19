725bb62f954e9c9ece5805522f9765514fc063e2
Vidyesh Churi <vidyesh95@gmail.com>
2023-09-20 00:24:33 +0530

    Conditions in Rust

--- conditions/src/main.rs
+++ conditions/src/main.rs
@@ -1,3 +1,29 @@
+/**
+* Conditions in Rust:
+* if/else expression allows us to branch our code depending on conditions.
+*/
 fn main() {
-    println!("Hello, world!");
+    println!("Conditions in rust");
+
+    // Simple condition
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    // Multiple conditions
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
 }
