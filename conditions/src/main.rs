c4ca4c6f1f3359b8fd927a7a3103471dc547692d
Vidyesh Churi <vidyesh95@gmail.com>
2023-09-20 15:29:38 +0530

    Conditions in Rust

--- conditions/src/main.rs
+++ conditions/src/main.rs
@@ -26,4 +26,10 @@ fn main() {
     } else {
         println!("number is not divisible by 4, 3, or 2");
     }
+
+    // Using if in a let statement
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {}", number);
 }
