7d9e08d98c0677fdc4d5172303e4b34a793626f8
Vidyesh Churi <vidyesh95@gmail.com>
2023-09-22 23:30:17 +0530

    Hello Cargo in Rust
    Delete hello_cargo/target directory

--- hello_cargo/target/debug/deps/hello_cargo-1072ffa7b5b54969.d
+++
@@ -1,5 +0,0 @@
-/Users/churi/code/language/rust/rust_tutorial/hello_cargo/target/debug/deps/hello_cargo-1072ffa7b5b54969.rmeta: src/main.rs
-
-/Users/churi/code/language/rust/rust_tutorial/hello_cargo/target/debug/deps/hello_cargo-1072ffa7b5b54969.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/churi/code/language/rust/rust_tutorial/hello_cargo/target/debug/hello_cargo: /Users/churi/code/language/rust/rust_tutorial/hello_cargo/src/main.rs
