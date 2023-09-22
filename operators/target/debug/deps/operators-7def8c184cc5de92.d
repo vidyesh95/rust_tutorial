97768b6b57534158ff874c2ebe4dd2b713e28fa5
Vidyesh Churi <vidyesh95@gmail.com>
2023-09-22 23:31:31 +0530

    Operators in Rust
    Delete operators/target directory

--- operators/target/debug/deps/operators-7def8c184cc5de92.d
+++
@@ -1,5 +0,0 @@
-/Users/churi/code/language/rust/rust_tutorial/operators/target/debug/deps/operators-7def8c184cc5de92: src/main.rs
-
-/Users/churi/code/language/rust/rust_tutorial/operators/target/debug/deps/operators-7def8c184cc5de92.d: src/main.rs
-
-src/main.rs:
--- operators/target/debug/operators.d
+++
@@ -1,1 +0,0 @@
-/Users/churi/code/language/rust/rust_tutorial/operators/target/debug/operators: /Users/churi/code/language/rust/rust_tutorial/operators/src/main.rs
