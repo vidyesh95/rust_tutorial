8524d4328fd8327d1c6e41abcbf847fe8d42bceb
Vidyesh Churi <vidyesh95@gmail.com>
2023-09-22 23:32:52 +0530

    Variables in Rust
    Delete variables/target directory

--- variables/target/debug/deps/variables-92d1977deda6e9f2.d
+++
@@ -1,5 +0,0 @@
-/Users/churi/code/language/rust/rust_tutorial/variables/target/debug/deps/variables-92d1977deda6e9f2: src/main.rs
-
-/Users/churi/code/language/rust/rust_tutorial/variables/target/debug/deps/variables-92d1977deda6e9f2.d: src/main.rs
-
-src/main.rs:
--- variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/Users/churi/code/language/rust/rust_tutorial/variables/target/debug/variables: /Users/churi/code/language/rust/rust_tutorial/variables/src/main.rs
