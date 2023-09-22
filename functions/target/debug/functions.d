49b5b0a1fb5f37cd29aed605273beeed56fe0194
Vidyesh Churi <vidyesh95@gmail.com>
2023-09-22 23:27:13 +0530

    Functions in Rust
    Delete datatypes/target directory

--- functions/target/debug/functions.d
+++
@@ -1,1 +0,0 @@
-/Users/churi/code/language/rust/rust_tutorial/functions/target/debug/functions: /Users/churi/code/language/rust/rust_tutorial/functions/src/main.rs
--- functions/target/release/functions.d
+++
@@ -1,1 +0,0 @@
-/Users/churi/code/language/rust/rust_tutorial/functions/target/release/functions: /Users/churi/code/language/rust/rust_tutorial/functions/src/main.rs
